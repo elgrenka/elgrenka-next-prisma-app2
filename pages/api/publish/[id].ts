a7d595350ff9c8fb5205a90172b9ae8329844c80
elgrenka <bugigen@yandex.ru>
2022-07-03 20:50:34 +0400

    ready

---
+++ pages/api/publish/[id].ts
@@ -0,0 +1,13 @@
+import prisma from '../../../lib/prisma';
+
+// PUT /api/publish/:id
+export default async function handle(req: any, res: any) {
+  const postId = req.query.id;
+  const post = await prisma.post.update({
+    where: { id: postId },
+    data: { published: true },
+  });
+  res.json(post);
+}
+
+
